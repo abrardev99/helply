0315e4928aee00cbbd001f83106f6eeac195fbdf
Abrar Ahmad <abrar.dev99@gmail.com>
2026-06-26 11:18:10 +0200

    initial status

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->configureDefaults();
+    }
+
+    /**
+     * Configure default behaviors for production-ready applications.
+     */
+    protected function configureDefaults(): void
+    {
+        $this->configureModels();
+
+        Date::use(CarbonImmutable::class);
+
+        DB::prohibitDestructiveCommands(
+            app()->isProduction(),
+        );
+
+        Password::defaults(fn (): ?Password => app()->isProduction()
+            ? Password::min(12)
+                ->mixedCase()
+                ->letters()
+                ->numbers()
+                ->symbols()
+                ->uncompromised()
+            : null,
+        );
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::unguard();
+
+        Relation::enforceMorphMap([
+            'user' => User::class,
+        ]);
+
+    }
+
+}
